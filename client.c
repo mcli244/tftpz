65e62a0f28c7174cb884781af074423012a2a153
lixiangjun <lixiangjun@abupdate.com>
2022-06-08 14:01:18 +0800

    [Feat] style changed

--- client.c
+++ client.c
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/time.h>
+#include <stdlib.h>
 #include "client.h"
 
 
@@ -26,21 +27,67 @@ struct tftp_info_typdef
     uint16_t timeout;   // 单位：秒
     char filename[256];
 };
-uint8_t rx_buf[2048] = {0};
-uint8_t tx_buf[512] = {0};
+
+#define RX_BUF_SIZE 2048
+#define TX_BUF_SIZE 512
+
+uint8_t g_rx_buf[RX_BUF_SIZE] = {0};
+uint8_t g_tx_buf[TX_BUF_SIZE] = {0};
 static struct tftp_info_typdef tftp_inf;
+
+void start_tftp_get(int sock_fd, struct sockaddr_in *server, struct tftp_info_typdef *inf)
+{
+    uint8_t *pkt = (uint8_t *)inf->tx_pkg;
+	uint8_t *xp;
+	int len = 0;
+	ushort *s;
+
+    /*
+      +-------+---~~---+---+---~~---+---+---~~---+---+---~~---+---+
+      |  opc  |filename| 0 |  mode  | 0 | blksize| 0 | #octets| 0 |
+      +-------+---~~---+---+---~~---+---+---~~---+---+---~~---+---+
+    */
+    xp = pkt;
+    s = (ushort *)pkt;
+    *s++ = htons(OPCODE_RRQ);
+    pkt = (uint8_t *)s;
+    strcpy((char *)pkt, inf->filename);
+    pkt += strlen(inf->filename) + 1;
+    strcpy((char *)pkt, "octet");
+    pkt += 5 /*strlen("octet")*/ + 1;
+    strcpy((char *)pkt, "timeout");
+    pkt += 7 /*strlen("timeout")*/ + 1;
+    sprintf((char *)pkt, "%u", inf->timeout);
+    pkt += strlen((char *)pkt) + 1;
+    pkt += sprintf((char *)pkt, "tsize%c%u%c",
+            0, inf->tsize, 0);
+    /* try for more effic. blk size */
+    pkt += sprintf((char *)pkt, "blksize%c%d%c",
+            0, inf->block_size, 0);
+    len = pkt - xp;
+
+    sendto(sock_fd, inf->tx_pkg, len, 0, (struct sockaddr *)server, sizeof(struct sockaddr_in));
+}
 
 int main(int argc, char **argv)
 {
     int sock_fd = -1;
     struct sockaddr_in server;
     struct sockaddr_in sender;
-    
-    uint8_t *pkt = (uint8_t *)&tx_buf;
-	uint8_t *xp;
+    socklen_t addr_len;
+	uint8_t *xp, *pkt;
 	int len = 0;
-	ushort *s;
+    ssize_t r_size = -1;
 
+
+    if(argc != 4)
+    {
+        printf("Usage: client [serverip] [port] [filename]\r\n"
+                "\teg:client 172.18.101.121 69 test.txt\r\n");
+        return 0;
+    }
+    
+    addr_len = sizeof(struct sockaddr_in);
     // UDP常用socket 创建
     // SOCK_DGRAM 数据报 套接字
     // IPPROTO_UDP 协议UDP
@@ -58,75 +105,38 @@ int main(int argc, char **argv)
     setsockopt(sock_fd, SOL_SOCKET, SO_BINDTODEVICE, (char *)&ifr, sizeof(ifr));
     
     /* 设置接收超时时间 */
-    struct timeval tv;
-    tv.tv_sec = 5;
-    tv.tv_usec = 0;
+    struct timeval tv = {10, 0};
     setsockopt(sock_fd, SOL_SOCKET, SO_RCVTIMEO, (char*)&tv, sizeof(struct timeval));
     
     // 服务器信息
     server.sin_family = AF_INET;
-    server.sin_port = htons(69);
-    server.sin_addr.s_addr = inet_addr("172.18.101.121");
+    server.sin_port = htons(atoi(argv[2]));
+    server.sin_addr.s_addr = inet_addr(argv[1]);
 
     tftp_inf.block_size = 512;
     tftp_inf.cur_block = 0;
     tftp_inf.timeout = 5;
-    tftp_inf.tx_pkg = tx_buf;
-    tftp_inf.rx_pkg = rx_buf;
+    tftp_inf.tx_pkg = g_tx_buf;
+    tftp_inf.rx_pkg = g_rx_buf;
     tftp_inf.tsize = 0;
+    strcpy(tftp_inf.filename, argv[3]);
 
+    printf("tftp start\r\n");
+    printf("serverip:%s port:%d\r\n",  inet_ntoa(server.sin_addr), ntohs(server.sin_port));
+    printf("filename:%s\r\n", tftp_inf.filename);
+    start_tftp_get(sock_fd, &server, &tftp_inf);
 
-    // 发送获取请求
-    struct tftp_info send_pkg, recv_pkg;
-    socklen_t addr_len;
-    addr_len = sizeof(struct sockaddr_in);
-
-    ssize_t r_size = -1;
-    int block = 0;
-    int tftp_block_size_option = 1468;
-    
-    /*
-      +-------+---~~---+---+---~~---+---+---~~---+---+---~~---+---+
-      |  opc  |filename| 0 |  mode  | 0 | blksize| 0 | #octets| 0 |
-      +-------+---~~---+---+---~~---+---+---~~---+---+---~~---+---+
-    */
-    memset(&tx_buf, 0x0, sizeof(tx_buf));
-
-    xp = pkt;
-    s = (ushort *)pkt;
-    *s++ = htons(OPCODE_RRQ);
-    pkt = (uint8_t *)s;
-    strcpy((char *)pkt, "Image");
-    pkt += strlen("Image") + 1;
-    strcpy((char *)pkt, "octet");
-    pkt += 5 /*strlen("octet")*/ + 1;
-    strcpy((char *)pkt, "timeout");
-    pkt += 7 /*strlen("timeout")*/ + 1;
-    sprintf((char *)pkt, "%u", 5);
-    pkt += strlen((char *)pkt) + 1;
-    pkt += sprintf((char *)pkt, "tsize%c%u%c",
-            0, 0, 0);
-    /* try for more effic. blk size */
-    pkt += sprintf((char *)pkt, "blksize%c%d%c",
-            0, tftp_block_size_option, 0);
-    len = pkt - xp;
-
-    sendto(sock_fd, &tx_buf, len, 0, (struct sockaddr *)&server, sizeof(struct sockaddr_in));
-    printf("send.. len:%d\r\n", len);
-
-    
     while(1)
     {
-        r_size = recvfrom(sock_fd, &rx_buf, sizeof(rx_buf), MSG_WAITFORONE, (struct sockaddr *)&sender, &addr_len);
+        r_size = recvfrom(sock_fd, tftp_inf.rx_pkg, RX_BUF_SIZE, MSG_WAITFORONE, (struct sockaddr *)&sender, &addr_len);
         if(r_size <= 2)
         {
-            printf("TIMEPUT\r\n");
-            usleep(10*1000);
-            continue;
+            printf("tftp recv timeout\r\n");
+            goto exit;
         }
         
-        uint16_t *s = (uint16_t *)&rx_buf;
-        printf("ntohs(*s):%d block:%d r_size:%ld\r\n", ntohs(*s), ntohs(*(s+1)), r_size);
+        uint16_t *s = (uint16_t *)tftp_inf.rx_pkg;
+        // printf("ntohs(*s):%d block:%d r_size:%ld\r\n", ntohs(*s), ntohs(*(s+1)), r_size);
         switch (ntohs(*s))
         {
         case OPCODE_RRQ     : 
@@ -134,38 +144,44 @@ int main(int argc, char **argv)
         case OPCODE_WRQ     : 
             break;
         case OPCODE_OACK    :
-            printf("OPCODE_OACK\r\n");
             /* 发送ACK */
-            pkt = (uint8_t *)&tx_buf;
+            pkt = (uint8_t *)tftp_inf.tx_pkg;
             xp = pkt;
             s = (ushort *)pkt;
             s[0] = htons(OPCODE_ACK);
-            s[1] = htons(block);
+            s[1] = htons(tftp_inf.cur_block);
             pkt = (uint8_t *)(s + 2);
             len = pkt - xp;
-            sendto(sock_fd, &tx_buf, len, 0, (struct sockaddr *)&sender, sizeof(struct sockaddr_in));
-            block++;
+            sendto(sock_fd, tftp_inf.tx_pkg, len, 0, (struct sockaddr *)&sender, sizeof(struct sockaddr_in));
+            tftp_inf.cur_block++;
+            printf("#");
+            fflush(stdout);
             break;
         case OPCODE_DATA    :
-            if(*(s+1) != htons(block))
+            if(*(s+1) != htons(tftp_inf.cur_block))
                 break;
-
+            if((tftp_inf.cur_block % 512) == 1)
+            {
+                printf("#");
+                fflush(stdout);
+            }
+                
             /* 发送ACK */
-            pkt = (uint8_t *)&tx_buf;
+            pkt = (uint8_t *)tftp_inf.rx_pkg;
             xp = pkt;
             s = (ushort *)pkt;
             s[0] = htons(OPCODE_ACK);
-            s[1] = htons(block);
+            s[1] = htons(tftp_inf.cur_block);
             pkt = (uint8_t *)(s + 2);
             len = pkt - xp;
-            sendto(sock_fd, &tx_buf, len, 0, (struct sockaddr *)&sender, sizeof(struct sockaddr_in));
-            if(r_size - 4 < tftp_block_size_option)
+            sendto(sock_fd, tftp_inf.rx_pkg, len, 0, (struct sockaddr *)&sender, sizeof(struct sockaddr_in));
+            if(r_size - 4 < tftp_inf.block_size)
             {
-                block = 0;
+                tftp_inf.cur_block = 0;
                 printf("recv succ\r\n");
                 goto exit;
             }else{
-                block++;
+                tftp_inf.cur_block++;
             }
             
             break;
@@ -177,12 +193,8 @@ int main(int argc, char **argv)
         default:
             break;
         }
-
-        //usleep(10*1000);
     }
     
-
-
 exit:
     close(sock_fd);
     return 0;
